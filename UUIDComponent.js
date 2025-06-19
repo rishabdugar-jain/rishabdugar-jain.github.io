3a8db58410d73e539568f973fb3a9effe6fd2096
rishabdugar-jain <kingrishabdugar@gmail.com>
2025-06-19 09:23:54 +0530

    Add files via upload

--- UUIDComponent.js
+++ UUIDComponent.js
@@ -6,6 +6,7 @@ export default class UUIDComponent {
     render(uuid) {
         const uuidTemplate = document.createElement("span");
         uuidTemplate.classList.add("uuid");
+        uuidTemplate.style.color = "#27c93f";
         uuidTemplate.innerHTML = `${uuid}`;
         this.container.appendChild(uuidTemplate);
     }
